6da52ff82fe8996b4b37a4137ea79a73bf6532f5
Vasu Devan <Vasu Devan>
2016-08-20 22:40:48 +0530

    added .py files from assignment1

--- Assignment1/EastWestAirlines.py
+++ Assignment1/EastWestAirlines.py
@@ -1,82 +1,110 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jul  3 20:17:01 2016
+Created on Sun Jun 26 12:47:52 2016
 
 @author: Vasu
 """
 
 from matplotlib import pyplot as plt
-from sklearn.preprocessing import StandardScaler
 from scipy.cluster.hierarchy import dendrogram, linkage, cut_tree
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import PCA
 
-df = pd.read_csv(r'C:\Users\Vasu\Business Analytics\Datamining1\Assignment\Assignment1\winedata.csv')
-df1 = df.copy()
-del df1['Wine']
-df1 = df1.set_index('ID')
-pca = PCA()
-df_scaled = StandardScaler().fit_transform(df1)
-pca.fit(df_scaled)
-# Plotting dendrogram using full data
-Z = linkage(df_scaled,method = 'ward')
-dendrogram(Z)
-# Plotting dendrogram using first two princ comp
-df_red = PCA(n_components=2).fit_transform(df_scaled)
-Zred = linkage(df_red, method = 'ward')
-dendrogram(Zred)
+df = pd.read_csv(r'C:\Users\Vasu\Business Analytics\Datamining1\Assignment\Assignment1\EastWestAirlinesCluster.csv')
+df.set_index('ID#')
+df_norm = (df - df.mean())/df.std()
+#df_norm = df.copy()
+Z = linkage(df_norm, 'ward')
 
-# Predicting clusters with cutree using full data
-cutreefull = cut_tree(Z, n_clusters=3)
-df_scaled.loc[:,'predicted'] = cutreefull
+plt.figure(figsize=(25, 10))
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('sample index')
+plt.ylabel('distance')
+dendrogram(
+    Z,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+b = dendrogram(Z, p=2, truncate_mode = 'lastp')
+nclust = 2
+cutree = cut_tree(Z, n_clusters=nclust)
+df.loc[:,'ClusterID'] = cutree
+dict1 = {}
+for i in range(nclust):
+    dict1["df_{0}".format(i)] = df[df.loc[:,'ClusterID'] == i]
+listcount = [dict1["df_{0}".format(i)].count()[1] for i in range(nclust)]
+listmean = [dict1["df_{0}".format(i)].mean() for i in range(nclust)]
+print (listmean)
+##################Part b#######################
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage, cut_tree
+import numpy as np
+import pandas as pd
 
-# Predicting clusters with cutree using first two princ comp
-cutreered = cut_tree(Zred, n_clusters=3)
-df_red = pd.DataFrame(df_red)
-df_red.loc[:,'predicted'] = cutreered
+df = pd.read_csv(r'C:\Users\Vasu\Business Analytics\Datamining1\Assignment\Assignment1\EastWestAirlinesCluster.csv')
+df.set_index('ID#')
+#df_norm = (df - df.mean())/df.std()
+df_norm = df.copy()
+Z = linkage(df_norm, 'ward')
 
-#TODO Measuring the accuracy of predictions
+plt.figure(figsize=(25, 10))
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('sample index')
+plt.ylabel('distance')
+dendrogram(
+    Z,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+b = dendrogram(Z, p=2, truncate_mode = 'lastp')
+nclust = 2
+cutree = cut_tree(Z, n_clusters=nclust)
+df.loc[:,'ClusterID'] = cutree
+dict1 = {}
+for i in range(nclust):
+    dict1["df_{0}".format(i)] = df[df.loc[:,'ClusterID'] == i]
+listcount = [dict1["df_{0}".format(i)].count()[1] for i in range(nclust)]
+listmean = [dict1["df_{0}".format(i)].mean() for i in range(nclust)]
+print (listmean)
+#######################Part d#################
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage, cut_tree
+import numpy as np
+import pandas as pd
+df = pd.read_csv(r'C:\Users\Vasu\Business Analytics\Datamining1\Assignment\Assignment1\EastWestAirlinesCluster.csv')
+retain = np.random.randint(0,3999,3800)
+df.set_index('ID#')
+df_norm = (df - df.mean())/df.std()
+retain = np.random.randint(0,3999,3800)
+df_norm = df_norm.loc[retain,:]
+#df_norm = df.copy()
+Z = linkage(df_norm, 'ward')
 
-#Taking two most significant chemicals
-pca1 = PCA().fit(df_scaled) 
-princcomp = pd.DataFrame(pca1.components_)
-vari = pca1.explained_variance_
-princcomp
-princcomp.mul(vari, axis=1)
+plt.figure(figsize=(25, 10))
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('sample index')
+plt.ylabel('distance')
+dendrogram(
+    Z,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+b = dendrogram(Z, p=2, truncate_mode = 'lastp')
+nclust = 2
+cutree = cut_tree(Z, n_clusters=nclust)
+df = df.loc[retain,:]
+df.loc[:,'ClusterID'] = cutree
+dict1 = {}
+for i in range(nclust):
+    dict1["df_{0}".format(i)] = df[df.loc[:,'ClusterID'] == i]
+listcount = [dict1["df_{0}".format(i)].count()[1] for i in range(nclust)]
+listmean = [dict1["df_{0}".format(i)].mean() for i in range(nclust)]
+print (listmean)
 
-# Correlation matrix
-corrmat = pd.DataFrame(np.corrcoef(df1.T))
-eig_vals, eig_vecs = np.linalg.eig(corrmat)
-
-# Make a list of (eigenvalue, eigenvector) tuples
-eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
-
-# Sort the (eigenvalue, eigenvector) tuples from high to low
-eig_pairs.sort(key=lambda x: x[0], reverse=True)
-
-# Visually confirm that the list is correctly sorted by decreasing eigenvalues
-print('Eigenvalues in descending order:')
-for i in eig_pairs:
-    print(i[0])
-
-princcomp = pd.DataFrame([eig_pairs[i][1] for i in range(len(eig_pairs))])
-princcomp.index = df1.columns
-
-
-# Plotting heatmap for correlation matrix
-
-fig, ax = plt.subplots()
-heatmap = ax.pcolor(corrmat,cmap = plt.cm.Greens)
-# put the major ticks at the middle of each cell
-ax.set_xticks(np.arange(corrmat.shape[0])+0.5, minor=False)
-ax.set_yticks(np.arange(corrmat.shape[1])+0.5, minor=False)
-
-# want a more natural, table-like display
-ax.invert_yaxis()
-ax.xaxis.tick_top()
-row_labels = df1.columns
-column_labels = df1.columns
-ax.set_xticklabels(row_labels, minor=False, rotation = 90)
-ax.set_yticklabels(column_labels, minor=False)
-print (fig)
+###############K-Means Clustering##################
+from sklearn.cluster import KMeans
+KM = KMeans(n_clusters=2, n_init=100, random_state=1,init='random',\
+    precompute_distances = False, verbose=True)
+    
+KM.fit(df_norm)
+listmean = [KM.cluster_centers_[i]*df.std() + df.mean() for i in range(nclust)]
